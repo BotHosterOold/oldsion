f4e5e326f2b52b1db01c0111655aac4640691789
BotHosterOold <50768383+BotHosterOold@users.noreply.github.com>
2019-06-09 19:45:45 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ bot.on("message", (message) => {
 
   if (message.content.toLowerCase().startsWith(prefix + `help`)) {
     const embed = new Discord.RichEmbed()
-    .setTitle(`:mailbox_with_mail: Vulnix Help`)
+    .setTitle(`Oldsion Help`)
     .setColor(0xCF40FA)
-    .setDescription(`Hello! I'm Vulnix, the Discord bot for super cool support ticket stuff and more! Here are my commands:`)
-    .addField(`Tickets`, `[${prefix}new]() > Opens up a new ticket and tags the Support Team\n[${prefix}close]() > Closes a ticket that has been resolved or been opened by accident`)
-    .addField(`Other`, `[${prefix}help]() > Shows you this help menu your reading\n[${prefix}ping]() > Pings the bot to see how long it takes to react\n[${prefix}about]() > Tells you all about Vulnix`)
+    .setDescription(`I am Oldsion , i am code by oold to extinct the virus`)
+    .addField(`?enable > Enable Oldsion`)
+    .addField(`?disable > Disable Oldsion`)
     message.channel.send({ embed: embed });
   }
 
