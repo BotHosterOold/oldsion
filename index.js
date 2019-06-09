353ab50e20d4defde53a9f4305e02bb875295e49
BotHosterOold <50768383+BotHosterOold@users.noreply.github.com>
2019-06-09 17:41:54 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,32 @@ bot.on('ready', () =>{
     console.log('Bot已經Online了喲！')
 })
 
-if (message.content.toLowerCase().startsWith(prefix + `enable`)) 
-        message.channel.send(`:white_check_mark: Enabled oldsion`);
+if (message.content.toLowerCase().startsWith(prefix + `enable`)) {
+    const embed = new Discord.RichEmbed()
+    .setTitle(`Oldsion Enabling and Disablement`)
+    .setColor(0xCF40FA)
+    .setDescription(`Notification:`)
+    .addField(`Enabled Oldsion`)
+    message.channel.send({ embed: embed });
+  }
 
-if (message.content.toLowerCase().startsWith(prefix + `disable`)) 
-        message.channel.send(`:white_check_mark: Disabled oldsion`);          
 
-if (message.content.toLowerCase().startsWith(prefix + `info`)) 
-        message.channel.send(`Hi, i am a anti-virus bot that made by oold. I am going to extinct the virus if i see them!`);
+if (message.content.toLowerCase().startsWith(prefix + `disable`)) {
+    const embed = new Discord.RichEmbed()
+    .setTitle(`Oldsion Enabling and Disablement`)
+    .setColor(0xCF40FA)
+    .setDescription(`Notification:`)
+    .addField(`Disabled Oldsion`)
+    message.channel.send({ embed: embed });
+  }         
+
+if (message.content.toLowerCase().startsWith(prefix + `disable`)) {
+    const embed = new Discord.RichEmbed()
+    .setTitle(`I am Oldsion`)
+    .setColor(0xCF40FA)
+    .setDescription(`Hi, i am a anti-virus bot that made by oold. I am going to extinct the virus if i see them!`)
+    .addField(`Have a good day)
+    message.channel.send({ embed: embed });
+  } 
     
 bot.login(token);
