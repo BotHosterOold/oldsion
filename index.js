f091cf9fd013bcc86c254c85df8e1f375209cfab
BotHosterOold <50768383+BotHosterOold@users.noreply.github.com>
2019-06-09 17:33:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ bot.on('ready', () =>{
     console.log('Bot已經Online了喲！')
 })
 
-if (message.content.toLowerCase().startsWith(prefix + `enable`)) {
-    message.channel.send(`:white_check_mark: Enabled oldsion`);}
+if (message.content.toLowerCase().startsWith(prefix + `enable`)) 
+        message.channel.send(`:white_check_mark: Enabled oldsion`);
 
-if (message.content.toLowerCase().startsWith(prefix + `disable`)) {
-    message.channel.send(`:white_check_mark: Disabled oldsion`);}
+if (message.content.toLowerCase().startsWith(prefix + `disable`)) 
+        message.channel.send(`:white_check_mark: Disabled oldsion`);}
 
-if (message.content.toLowerCase().startsWith(prefix + `info`)) {
-    message.channel.send(`Hi, i am a anti-virus bot that made by oold. I am going to extinct the virus if i see them!`);}
+if (message.content.toLowerCase().startsWith(prefix + `info`)) 
+        message.channel.send(`Hi, i am a anti-virus bot that made by oold. I am going to extinct the virus if i see them!`);
     
 bot.login(token);
