41c6a514e96d2c5e44b97b14c27f47728e02d0ce
BotHosterOold <50768383+BotHosterOold@users.noreply.github.com>
2019-06-09 19:54:10 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ bot.on("message", (message) => {
     .setTitle(`Oldsion Help`)
     .setColor(0xCF40FA)
     .setDescription(`I am Oldsion , i am code by oold to extinct the virus`)
-    .addField(`?enable > Enable Oldsion`)
-    .addField(`?disable > Disable Oldsion`)
+    .addField(`Mode`, `[${prefix}enable]() > Enable Oldsion`)
+    .addField(`Mode`, `[${prefix}disable]() > Disable Oldsion`)
     message.channel.send({ embed: embed });
   }
 
