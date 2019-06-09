744b2cfb5b8b4d6a9c7c8c8f6ea97f39e3662528
BotHosterOold <50768383+BotHosterOold@users.noreply.github.com>
2019-06-09 22:17:43 +0800

    Update index.js

--- index.js
+++ index.js
@@ -104,4 +104,29 @@ if (message.content.toLowerCase().startsWith(prefix + `close`)) {
 
 });
 
+exports.run = (guild, message, args) => {
+    let modRole = message.guild.roles.find("name", "【服主】OWNER");
+    if(message.member.roles.has(modRole.id)) {
+        let reason = args.slice(1).join(' ');
+        let user = message.mentions.users.first();
+        let modlog = guild.channels.find('name', 'logs');
+
+        if (!modlog) 
+            return message.reply('I cannot find a mod-log channel');
+
+        if (reason.length < 1) 
+            return message.reply('You must supply a reason for the warning.');
+
+        if (message.mentions.users.size < 1) 
+            return message.reply('You must mention someone to warn them.').catch(console.error);
+
+        const embed = new Discord.RichEmbed()
+            .setColor(0x8cff00)
+            .setTimestamp()
+            .setDescription(`**Action:** Warning\n**Target:** ${user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`);
+
+        return guild.channels.get(modlog.id).send({embed});
+    }
+};
+
 bot.login(token);
